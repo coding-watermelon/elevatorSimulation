1f2ba724c9a83bd8fefcd8f14fad065e6bcf66f5
Willi Gierke <willi.gierke@student.hpi.uni-potsdam.de>
2016-09-17 08:44:39 +0200

    added gaussian distribution generation

--- data_generator.js
+++ data_generator.js
@@ -3,6 +3,36 @@
 const MAX_NUMBER_OF_PEOPLE = 16;
 const ELEVATOR_SPEED = 2
 
+// returns a gaussian random function with the given mean and standard deviation
+function gaussian(mean, stdev) {
+    var y2;
+    var use_last = false;
+    return function() {
+        var y1;
+        if(use_last) {
+           y1 = y2;
+           use_last = false;
+        }
+        else {
+            var x1, x2, w;
+            do {
+                 x1 = 2.0 * Math.random() - 1.0;
+                 x2 = 2.0 * Math.random() - 1.0;
+                 w  = x1 * x1 + x2 * x2;               
+            } while( w >= 1.0);
+            w = Math.sqrt((-2.0 * Math.log(w))/w);
+            y1 = x1 * w;
+            y2 = x2 * w;
+            use_last = true;
+       }
+
+       var retval = mean + stdev * y1;
+       if(retval > 0) 
+           return retval;
+       return -retval;
+   }
+}
+
 function generatePeople(count){
   let people = []
   
@@ -14,14 +44,14 @@ function generatePeople(count){
     // Time represented as number of minutes after 0 o clock 
     // is number between 0 and 1440
     // Starttime between 8 o clock and 10 o clock
-    person.workStartTime = rand(480, 600) 
+    person.workStartTime = gaussian(540, 60) 
     
     // Between 12 am and 2 pm
-    person.breakStartTime = rand(720, 840)
+    person.breakStartTime = gaussian(780, 60)
     person.breakStopTime = person.breakStartTime + 45
 
     // Between 8 and 10 hours after start
-    person.workStopTime = person.workStartTime + rand(480, 600)
+    person.workStopTime = person.workStartTime + gaussian(540, 60)
 
     people.push(person)
   }
@@ -70,6 +100,3 @@ function generateElevator(count){
 
   return elevators
 }
-
-
-
