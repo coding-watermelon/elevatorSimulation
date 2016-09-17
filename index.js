3034c57e38d0b592d1f0f8875ce545bc1196f42e
Stephan Schultz <register@steppschuh.net>
2016-09-17 09:39:03 +0200

    Added index.html

---
+++ index.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function(){ 
+  initialize();
+}, false);
+
+function initialize() {
+  console.log("Initializing");
+  
+  var canvas = document.getElementById("canvas");
+  renderer.initializeCanvas(canvas);
+  
+}
