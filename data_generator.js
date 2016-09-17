9b903017fe2a8a203345b4d0d4e877209cb0c6ee
Markus Petrykowski <github@markus-petrykowski.de>
2016-09-17 09:17:42 +0200

    turn generator into singelton

--- data_generator.js
+++ data_generator.js
@@ -1,106 +1,122 @@
 'use strict'
 
-const MAX_NUMBER_OF_PEOPLE = 16;
-const ELEVATOR_SPEED = 2
+var generator = function(){
+  const MAX_NUMBER_OF_PEOPLE = 16;
+  const ELEVATOR_SPEED = 2
 
-function generatePeople(count){
-  let people = []
-  
+  let generator = {}
 
-  for(var i=0; i<count; i++){
-    let person = {}
-    person.id = i
-
-    // Time represented as number of minutes after 0 o clock 
-    // is number between 0 and 1440
-    // Starttime between 8 o clock and 10 o clock
-    person.workStartTime = gaussian(540, 60) 
-    
-    // Between 12 am and 2 pm
-    person.breakStartTime = gaussian(780, 60)
-    person.breakStopTime = person.breakStartTime + 45
-
-    // Between 8 and 10 hours after start
-    person.workStopTime = person.workStartTime + gaussian(540, 60)
-
-    people.push(person)
-  }
-
-  return people
-}
-
-function rand(minMin, maxMin) {
-  minMin = minMin * 60 * 1000
-  maxMin = maxMin * 60 * 1000
-
-  return Math.floor(Math.random() * (maxMin - minMin + 1)) + minMin;
-}
-
-function generateLevel(count){
-  let level = []
-
-  for(var i=0; i<count; i++){
-    let currLevel = {
-      id: i,
-      people: [],
-      elevatorUpRequested: false,
-      elevatorDownRequested: false
+  generator.init = function(peopleCount, elevatorCount, levelCount){
+    return {
+      people: generatePeople(peopleCount),
+      elevator: generateElevator(elevatorCount),
+      level: generateLevel(levelCount)
     }
-    level.push(currLevel)
   }
 
-  return level
-}
-
-function generateElevator(count){
-  let elevators = []
-
-  for(var i=0; i<count; i++){
-    let elevator = {
-      id: i,
-      maximumNumberOfPeople: MAX_NUMBER_OF_PEOPLE,
-      people: [],
-      currLevel: 0,
-      targetLevels: [],
-      speed: ELEVATOR_SPEED,
-      waitTimeout: 0
-    }
-    elevators.push(elevator)
-  }
-
-  return elevators
-}
-
-
-// returns a gaussian random function with the given mean and standard deviation
-function gaussian(mean, stdev) {
-    mean = mean * 60 * 1000;
-    stdev = stdev * 60 * 1000;
+  function generatePeople(count){
+    let people = []
     
-    var y2;
-    var use_last = false;
-    return function() {
-        var y1;
-        if(use_last) {
-           y1 = y2;
-           use_last = false;
+
+    for(var i=0; i<count; i++){
+      let person = {}
+      person.id = i
+
+      // Time represented as number of minutes after 0 o clock 
+      // is number between 0 and 1440
+      // Starttime between 8 o clock and 10 o clock
+      person.workStartTime = gaussian(540, 60) 
+      
+      // Between 12 am and 2 pm
+      person.breakStartTime = gaussian(780, 60)
+      person.breakStopTime = person.breakStartTime + 45
+
+      // Between 8 and 10 hours after start
+      person.workStopTime = person.workStartTime + gaussian(540, 60)
+
+      people.push(person)
+    }
+
+    return people
+  }
+
+  function rand(minMin, maxMin) {
+    minMin = minMin * 60 * 1000
+    maxMin = maxMin * 60 * 1000
+
+    return Math.floor(Math.random() * (maxMin - minMin + 1)) + minMin;
+  }
+
+  function generateLevel(count){
+    let level = []
+
+    for(var i=0; i<count; i++){
+      let currLevel = {
+        id: i,
+        people: [],
+        elevatorUpRequested: false,
+        elevatorDownRequested: false
+      }
+      level.push(currLevel)
+    }
+
+    return level
+  }
+
+  function generateElevator(count){
+    let elevators = []
+
+    for(var i=0; i<count; i++){
+      let elevator = {
+        id: i,
+        maximumNumberOfPeople: MAX_NUMBER_OF_PEOPLE,
+        people: [],
+        currLevel: 0,
+        targetLevels: [],
+        speed: ELEVATOR_SPEED,
+        waitTimeout: 0
+      }
+      elevators.push(elevator)
+    }
+
+    return elevators
+  }
+
+
+  // returns a gaussian random function with the given mean and standard deviation
+  function gaussian(mean, stdev) {
+      mean = mean * 60 * 1000;
+      stdev = stdev * 60 * 1000;
+      
+      var y2;
+      var use_last = false;
+      return function() {
+          var y1;
+          if(use_last) {
+            y1 = y2;
+            use_last = false;
+          }
+          else {
+              var x1, x2, w;
+              do {
+                  x1 = 2.0 * Math.random() - 1.0;
+                  x2 = 2.0 * Math.random() - 1.0;
+                  w  = x1 * x1 + x2 * x2;               
+              } while( w >= 1.0);
+              w = Math.sqrt((-2.0 * Math.log(w))/w);
+              y1 = x1 * w;
+              y2 = x2 * w;
+              use_last = true;
         }
-        else {
-            var x1, x2, w;
-            do {
-                 x1 = 2.0 * Math.random() - 1.0;
-                 x2 = 2.0 * Math.random() - 1.0;
-                 w  = x1 * x1 + x2 * x2;               
-            } while( w >= 1.0);
-            w = Math.sqrt((-2.0 * Math.log(w))/w);
-            y1 = x1 * w;
-            y2 = x2 * w;
-            use_last = true;
-       }
 
-       var retval = mean + stdev * y1;
-       if(retval > 0) 
-           return Math.round(retval);
-       return -Math.round(retval);
-   }
-}
+        var retval = mean + stdev * y1;
+        if(retval > 0) 
+            return Math.round(retval);
+        return -Math.round(retval);
+    }
+  }
+
+  return generator;
+}()
+
+console.log( generator.init(3,3,3) )
