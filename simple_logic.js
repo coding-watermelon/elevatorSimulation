3a83bbbd50eb7fe0e58e9d9244ba033c392ecc24
Willi Gierke <willi.gierke@student.hpi.uni-potsdam.de>
2016-09-17 21:00:07 +0200

    added simple logic

--- simple_logic.js
+++ simple_logic.js
@@ -1,11 +1,45 @@
-'use strict';
+var simpleLogic = function(){
 
-let logic = function(){
+  var logic = {
+  };
 
-  let logic = {
-    requestElevatorDown: function(level){},
-    requestElevatorUp: function(level){}
+  function getNearestElevatorIndex(level) {
+    var nearestElevatorIndex = 0
+    var minimalDistance = Math.abs(looper.state.elevators[nearestElevatorIndex].currentLevel - level)
+    for (var i = 0; i < looper.state.elevators.length; i++) {
+        if(Math.abs(looper.state.elevators[i].currentLevel - level) < minimalDistance) {
+            minimalDistance = Math.abs(looper.state.elevators[i].currentLevel - level)
+            nearestElevatorIndex = i
+        }
+    }
+    console.log('Nearest elevator for level ' + level + ' is elevator ' + nearestElevatorIndex)
+    return nearestElevatorIndex
   }
 
-  return logic
-}()
+  logic.onElevatorUpRequested = function(level) {
+    //send closest unused elevator or elevator that will visit the level anyways
+    console.log("onElevatorUpRequested");
+    looper.state.elevators[getNearestElevatorIndex].addTargetLevel(level.id);
+  }
+
+  logic.onElevatorDownRequested = function(level) {
+    //send closest unused elevator or elevator that will visit the level anyways
+    console.log("onElevatorDownRequested");
+    looper.state.elevators[getNearestElevatorIndex].addTargetLevel(level.id);
+  }
+
+  logic.onTargetLevelsChanged = function(currentState, elevator, targetLevels) {
+    //go to target levels one after another
+  }
+
+  logic.onElevatorIdle = function(elevator) {
+    //stay on the level
+    //console.log("onElevatorIdle");
+  }
+
+  logic.onElevatorStopped = function(elevator) {
+    //console.log("onElevatorStopped");
+  }
+
+  return logic;
+}();
